41949906445c5d0fe61376e2c3e8461ad63ba6c5
AurimasG1 <112963534+AurimasG1@users.noreply.github.com>
2025-08-09 16:17:51 +0300

    Project Completed, fixed app.all('/{*any}'

--- backend/server.js
+++ backend/server.js
@@ -25,7 +25,7 @@ if (process.env.NODE_ENV === 'production') {
 	app.use(express.static(path.join(__dirname, '/frontend/dist')));
 
 	// ALL other requests server frontend
-	app.get('/*', (req, res) => {
+	app.get('/{*any}', (req, res) => {
 		res.sendFile(path.resolve(__dirname, 'frontend', 'dist', 'index.html'));
 	});
 } else {
